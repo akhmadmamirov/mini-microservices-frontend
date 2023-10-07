6d1f0ab93371b856f06edbf60ac968551bddfa1d
Akhmadillo Mamirov <akhmadillo1@gmail.com>
2023-10-06 20:14:25 -0400

    adding postCreate

---
+++ src/PostCreate.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+
+function PostCreate() {
+    
+    const [title, setTitle] = useState('');
+    
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        
+        await axios.post("http://localhost:4000/posts", {
+            title
+        });
+
+        setTitle("")
+    }
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <label htmlFor="">Title</label>
+                    <input 
+                        value={title} 
+                        onChange={e => setTitle(e.target.value)}
+                        className="form-control">
+                    </input>
+                </div>
+
+                <button className='btn btn-primary mt-3'> Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default PostCreate
